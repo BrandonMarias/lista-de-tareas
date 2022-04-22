ef183be14294203ed343cfb310f26a52f224fcde
BrandonMarias <80177652+BrandonMarias@users.noreply.github.com>
2022-04-22 14:04:47 -0500

    arregla los indices de la lista de tareas

--- models/tareas.js
+++ models/tareas.js
@@ -32,49 +32,48 @@ class Tareas {
     return listado;
   }
 
-  listadoCompleto() {
-    // const tareas = this.listadoArr;
-    // let lista = '';
+  // listadoCompleto() {
+  //   // const tareas = this.listadoArr;
+  //   // let lista = '';
 
-    // for (let i = 0; i< tareas.length; i++) {
-    // lista += `${`${i+1}.`.green} ${tareas[i].description} :: ${tareas[i].completdoEn? `completado`.green : `incompleta`.red} \n`
-    // }
+  //   // for (let i = 0; i< tareas.length; i++) {
+  //   // lista += `${`${i+1}.`.green} ${tareas[i].description} :: ${tareas[i].completdoEn? `completado`.green : `incompleta`.red} \n`
+  //   // }
 
-    // console.log(lista)
+  //   // console.log(lista)
 
-    console.log();
-    this.listadoArr.forEach((tarea, i) => {
-      const idx = `${i + 1}.`.green;
+  //   console.log();
+  //   this.listadoArr.forEach((tarea, i) => {
+  //     const idx = `${i + 1}.`.green;
+  //     const { description, completadoEn } = tarea;
+  //     const estado = completadoEn ? `completada`.green : `incompleta`.red;
+
+  //     console.log(`${idx} ${description} :: ${estado}`);
+  //   });
+  // }
+
+  listarTareas(completadas = null) {
+    let idx = 1;
+
+    this.listadoArr.forEach(tarea => {
       const { description, completadoEn } = tarea;
       const estado = completadoEn ? `completada`.green : `incompleta`.red;
 
-      console.log(`${idx} ${description} :: ${estado}`);
+      if (completadas === null) {
+        //regresa listado completo
+        console.log(`${idx}. ${description} :: ${estado}`);
+        idx++;
+      } else if (completadas && completadoEn) {
+        // regresa listado de completadas
+        console.log(`${idx}. ${description} :: ${estado}`);
+        idx++;
+      } else if (!completadas && !completadoEn) {
+        // regresa estado de incompletas
+        console.log(`${idx}. ${description} :: ${estado}`);
+        idx++;
+      }
     });
   }
-
-
-  listarTareas( completadas = null){
-
-    
-    this.listadoArr.forEach((tarea, i) => {
-      const idx = `${i + 1}.`.green;
-      const { description, completadoEn } = tarea;
-      const estado = completadoEn ? `completada`.green : `incompleta`.red;
-        
-        if (completadas === null) {//regresa listado completo
-          console.log(`${idx} ${description} :: ${estado}`);
-          
-        }
-        else if (completadas && completadoEn) {// regresa listado de completadas
-          console.log(`${idx} ${description} :: ${estado}`);
-        }
-        else if (!completadas && !completadoEn) {// regresa estado de incompletas
-          console.log(`${idx} ${description} :: ${estado}`);
-        }
-    });
-    
-  }
-
 }
 
 module.exports = Tareas;
